5095a64ae550a39ab888e5e31ba6573685b9fa85
FlancyE <132045261+FlancyE@users.noreply.github.com>
2023-12-27 22:44:59 +0800

    a

---
+++ 10699_Count_the_factor.py
@@ -0,0 +1,17 @@
+#10699 Count the factor
+def count_factor(n):
+    cnt = 0
+    for i in range(2, n):
+        if n % i == 0:
+            cnt += 1 
+
+    return
+
+while True:
+    try:
+        n = int(input())
+        print(count_factor(n))
+        if n == 0:
+            break
+    except EOFError:
+        break
