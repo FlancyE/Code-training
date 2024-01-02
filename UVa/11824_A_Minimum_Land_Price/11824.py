649480d415112d4453f6b9674e2fc6f37dd10802
FlancyE <132045261+FlancyE@users.noreply.github.com>
2024-01-02 08:42:44 +0000

    asd

---
+++ UVa/11824_A_Minimum_Land_Price/11824.py
@@ -0,0 +1,18 @@
+#11824 A minimum land price
+def count_price(nums):
+    sum = 0
+    nums = sorted(nums)[::-1]   #不太懂
+    for i in range(len(nums)):
+        sum += 2 * (nums[i] ** (i+1))
+    return sum if sum < 5000000 else "Too expensive"
+
+T = int(input())
+nums = []
+for _ in range(T):
+    nums = []
+    while True:
+        N = int(input())
+        if N == 0:
+            print(count_price(nums))
+            break
+        nums.append(N)
