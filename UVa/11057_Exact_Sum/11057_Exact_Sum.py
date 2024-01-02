2bc56ec71721e081b8bdf882eac916fbda895055
FlancyE <132045261+FlancyE@users.noreply.github.com>
2024-01-02 15:15:30 +0800

    upload 11057

---
+++ UVa/11057_Exact_Sum/11057_Exact_Sum.py
@@ -0,0 +1,28 @@
+while True:
+    try:
+        n = int(input())
+        prices = list(map(int, input().split()))    #regular begining
+        money = int(input())
+
+        prices.sort()
+
+        i, j = 0, n - 1
+        best_i, best_j = 0, 0   #set values of prices
+
+        while i < j:    #Binary search
+            if prices[i] + prices[j] == money:
+                best_i, best_j = i, j
+                i += 1
+                j -= 1
+            elif prices[i] + prices[j] < money:
+                i += 1
+            else:
+                j -= 1
+                
+        print(f"Peter should buy books whose prices are {prices[best_i]} and {prices[best_j]}.")
+        print()
+        input()
+
+    except EOFError:
+        break
+            
