6ae324e1342dccd405012dff5a59fc9f31220654
FlancyE <132045261+FlancyE@users.noreply.github.com>
2024-01-04 02:40:00 +0000

    qweqwe

--- UVa/11824_A_Minimum_Land_Price/11824.py
+++ UVa/11824_A_Minimum_Land_Price/11824.py
@@ -1,19 +1,19 @@
 #11824 A minimum land price
 
 def count_price(nums):
-    sum = 0
-    nums.sort(reverse=True)   #不太懂，由大到小整理
-    for i in range(len(nums)):
-        sum += 2 * (nums[i] ** (i+1))
-    return sum if sum < 5000000 else "Too expensive"
+	sum = 0
+	nums.sort(reverse=True)
+	for i in range(len(nums)):
+		sum += 2 * (nums[i] ** (i + 1))
+	return sum if sum < 5000000 else "Too expensive"
 
 T = int(input())
 nums = []
 for _ in range(T):
-    nums = []
-    while True:
-        N = int(input())
-        if N == 0:
-            print(count_price(nums))
-            break
-        nums.append(N)
+	nums = []
+	while True:
+		L = int(input())
+		if L == 0:
+			print(count_price(nums))
+			break
+		nums.append(L)
