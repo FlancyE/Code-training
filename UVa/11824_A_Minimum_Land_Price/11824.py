3f0316f83c69fabd8da0e7c1c9abd3f1b30760ab
FlancyE <132045261+FlancyE@users.noreply.github.com>
2024-01-08 14:23:08 +0000

    asd

--- UVa/11824_A_Minimum_Land_Price/11824.py
+++ UVa/11824_A_Minimum_Land_Price/11824.py
@@ -1,8 +1,8 @@
 #11824 A minimum land price
 
 def count_price(nums):
+	nums = sorted(nums)[::-1]	#由大到小排序，為了得到最小的總和 (一般是由小到大)
 	sum = 0
-	nums.sort(reverse=True)     #由大到小排序，為了得到最小的總和 (一般是由小到大)
 	for i in range(len(nums)):
 		sum += 2 * (nums[i] ** (i + 1))     # 2 * (X^i)
 	return sum if sum < 5000000 else "Too expensive"   
