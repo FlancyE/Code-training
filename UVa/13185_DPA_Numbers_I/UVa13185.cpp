4c3955c6cbed88973d8a7d9c0362f4db1540c47e
FlancyE <132045261+FlancyE@users.noreply.github.com>
2024-04-22 17:30:37 +0800

    13185

---
+++ UVa/13185_DPA_Numbers_I/UVa13185.cpp
@@ -0,0 +1,39 @@
+//UVa13185 by ChatGPT
+#include <bits/stdc++.h>
+using namespace std;
+
+int Factorization(int n){
+	int sum = 0;
+	for(int i = 1; i < n; i++){
+		if(n % i == 0){
+			sum += i;
+		}
+	}
+	return sum;
+}
+
+int main(){
+	int t;
+	cin >> t;
+	vector<int> B(t);
+
+	for(int i = 0; i < t; i++){
+		cin >> B[i];		//依序寫入測試值
+	}
+
+	for(int i = 0; i < t; i++){
+		int n = B[i];
+		int sum = Factorization(n);
+
+		if(sum < n){
+			cout << "deficient" << endl;
+		} else if(sum == n){
+			cout << "perfect" << endl;
+		} else{
+			cout << "abundant" << endl;
+		}
+	}
+	return 0;
+}
+
+//C++11 AC 0.000(?
